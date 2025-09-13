b7ed50285b7f537571103d882432523b17c07c13
Arbaz <1arbaaziconic@gmail.com>
2025-09-14 01:59:31 +0530

    warn init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -5,3 +5,9 @@ CREATE DATABASE datawarehouse;
 CREATE SCHEMA bronze;
 CREATE SCHEMA silver;
 CREATE SCHEMA gold;
+/*
+WARNING:
+    Running this script will drop the entire 'datawarehouse' database if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
+*/
