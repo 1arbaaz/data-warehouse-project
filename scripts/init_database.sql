b5aab2b5e7e91900e9c707b8980802eff20ec2bd
Arbaz <1arbaaziconic@gmail.com>
2025-09-22 12:00:09 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,13 @@
+DROP DATABASE IF EXISTS datawarehouse;
 
+CREATE DATABASE datawarehouse;
+
+CREATE SCHEMA bronze;
+CREATE SCHEMA silver;
+CREATE SCHEMA gold;
+/*
+WARNING:
+    Running this script will drop the entire 'datawarehouse' database if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
+*/
